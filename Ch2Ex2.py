b2ae04703866d98b28f2363968d58358c5d3324a
Jay Karna <Jayant_Karna@Comcast.com>
2016-12-29 12:28:07 -0500

    Initial Project Version.

---
+++ Ch2Ex2.py
@@ -0,0 +1,12 @@
+# "Table" heading
+print('{:>5} {:>5} {:>5}'.format('i |', 'i^2 |', 'i^3'))
+print('------------------')
+
+# Cycle i through values 1 - 10.  Print i, i^2, and i^3 (converted to strings for formatting purposes)
+for i in range (1, 10 + 1):
+
+	#Use string.format() to print columns.  ":>"" means using right alignment, "5" is the spacing between right alignments
+	print('{:>5} {:>5} {:>5}'.format(str(i) + ' |', str(i**2) + ' |', str(i**3)))
+
+#Require user input to close
+input()
