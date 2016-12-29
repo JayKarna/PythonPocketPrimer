0b84508689d6b3fc8a4ac9d9c86d45a46a877485
Jay Karna <Jayant_Karna@Comcast.com>
2016-12-29 11:37:10 -0500

    Initial Project Version

---
+++ Ch1Ex2.py
@@ -0,0 +1,27 @@
+#ONLY WORKS IN PYTHON 3.X.X
+
+#NOTE: Does not accept decimal values.
+
+
+while(1):
+	c = input('Input a temperature in Celsius: ')
+	try:
+		int(c)
+		break
+	except:
+		print('I\'m sorry, but ' + c + ' is not a valid temperature')
+
+cInF = (int(c) * (9/5)) + 32
+print('This temperature, in Fahrenheit, is ' + str(cInF))
+
+
+while(1):
+	f = input('Input a temperature in Fahrenheit: ')
+	try:
+		int(f)
+		break
+	except:
+		print('I\'m sorry, but ' + f + ' is not a valid temperature')
+	
+fInC = (int(f) - 32) * (5/9)
+print('This temperature, in Celsius, is ' + str(fInC))
